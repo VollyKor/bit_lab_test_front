3677dd6bcc3439776e7c9c27aa7415082475084f
Warness <volydemar2032@gmail.com>
2021-04-16 12:35:31 +0300

    fix query

--- src/components/UserPage.jsx
+++ src/components/UserPage.jsx
@@ -12,64 +12,66 @@ import {
 } from 'recharts';
 
 export default function SinglePage() {
-  const [data, setData] = useState([]);
-  const [user, setUser] = useState({});
+  const [data, setData] = useState(null);
+  const [user, setUser] = useState(null);
 
   const match = useRouteMatch();
 
   // const { first_name, last_name } = userData;
   const { id = 1 } = match.params;
+  const numberId = Number(id);
+  console.log(numberId);
 
   useEffect(() => {
-    getDataById(id).then(data => {
-      console.log(data);
-      setData(data.data);
-      setUser(data.data[0]);
-      const newDate = Date.parse(user.date);
-      console.log(newDate);
-    });
+    if (typeof numberId !== NaN) {
+      getDataById(id).then(data => {
+        console.log(data.data[0]);
+        setData(data.data);
+        setUser(data.data[0]);
+      });
+    }
   }, [id]);
 
-  return (
-    user && (
-      <>
-        <div>
-          <h2 className="user__title">{`${user.first_name} ${user.last_name}`}</h2>
-        </div>
-        <div className="graph">
-          <h3 className="user__subtitle">Clicks</h3>
-          <LineChart width={1000} height={400} data={data}>
-            <CartesianGrid vertical={false} />
-            <Line
-              type="monotone"
-              dataKey="total_click"
-              strokeWidth={4}
-              stroke="#3A80BA"
-            />
-            <Tooltip />
-            <Legend />
-            <XAxis dataKey="date" />
-            <YAxis dataKey="total_click" />
-          </LineChart>
-        </div>
-        <div className="graph">
-          <h3 className="user__subtitle">Vievs</h3>
-          <LineChart width={1000} height={400} data={data}>
-            <CartesianGrid vertical={false} stroke="#F1F1F1" />
-            <Line
-              type="monotone"
-              strokeWidth={4}
-              legendType="plainline"
-              dataKey="total_page_views"
-              stroke="#3A80BA"
-            />
-            <Tooltip />
-            <Legend />
-            <XAxis dataKey="date" />
-            <YAxis dataKey="total_page_views" />
-          </LineChart>
-        </div>
-      </>
-    )
+  return user ? (
+    <>
+      <div>
+        <h2 className="user__title">{`${user.first_name} ${user.last_name}`}</h2>
+      </div>
+      <div className="graph">
+        <h3 className="user__subtitle">Clicks</h3>
+        <LineChart width={1000} height={400} data={data}>
+          <CartesianGrid vertical={false} stroke="#F1F1F1" />
+          <Line
+            type="monotone"
+            dataKey="total_click"
+            strokeWidth={4}
+            stroke="#3A80BA"
+          />
+          <Tooltip />
+          <Legend />
+          <XAxis dataKey="date" />
+          <YAxis dataKey="total_click" />
+        </LineChart>
+      </div>
+      <div className="graph">
+        <h3 className="user__subtitle">Vievs</h3>
+        <LineChart width={1000} height={400} data={data}>
+          <CartesianGrid vertical={false} stroke="#F1F1F1" />
+          <Line
+            type="monotone"
+            strokeWidth={4}
+            legendType="plainline"
+            dataKey="total_page_views"
+            stroke="#3A80BA"
+          />
+          <Tooltip />
+          <Legend />
+          <XAxis dataKey="date" />
+          <YAxis dataKey="total_page_views" />
+        </LineChart>
+      </div>
+    </>
+  ) : (
+    <div>UserId is not a valid number or data is absent</div>
   );
 }
--- src/service/axios.js
+++ src/service/axios.js
@@ -1,15 +1,14 @@
 import axios from 'axios';
 
+// axios.defaults.baseURL = 'https://bit-lab-test.herokuapp.com';
+axios.defaults.baseURL = 'http://localhost:3010';
+
 export const getAll = async (page = 1, limit = 50) => {
-  const { data } = await axios.get(
-    `https://bit-lab-test.herokuapp.com/stats?page=${page}&limit=${limit}`,
-  );
-  return data.data;
+  const { data } = await axios.get(`/stats?page=${page}&limit=${limit}`);
+  return data;
 };
 
 export const getDataById = async userId => {
-  const { data } = await axios.get(
-    `https://bit-lab-test.herokuapp.com/stats/${userId}`,
-  );
-  return data.data;
+  const { data } = await axios.get(`/stats/${userId}`);
+  return data;
 };
