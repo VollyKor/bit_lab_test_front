54b1defd285a0053f677958b82a031177f1e95c0
VolKor <vol.kor.93.@gamil.com>
2021-04-21 13:36:18 +0300

    fix styles for form, update build

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,7 +2,11 @@ export default function Form() {
   return (
     <form className="form">
       <label className="form__label">
-        <input className="form__input" type="email" placeholder="Subscribe" />
+        <input
+          className="form__input"
+          type="email"
+          placeholder="example@email.com"
+        />
       </label>
       <button className="form__button" type="button">
         Subscribe
